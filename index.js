2037307ff48eeedd54503e13158cb978fcb9bc2e
Kenneth John <kennethjohnzabate@gmail.com>
2025-09-10 05:05:01 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,71 @@
+import express from "express";
+import bodyParser from "body-parser";
+import fs, { existsSync } from "fs";
+
+const app = express();
+const port = 3000;
+const POST_file = "post.json";
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+app.set("view engine", "ejs");
+
+function readPost(){
+    if(!fs.existsSync(POST_file)) return [];
+    try{
+        const data = fs.readFileSync(POST_file,["utf8"]);
+        return data ? JSON.parse(data): [];
+    }catch(err){
+        console.log("Error reading post.json",err);
+        return [];
+    }
+}
+
+function writePost(posts){
+    fs.writeFileSync(POST_file ,JSON.stringify(posts,null,2));
+}
+
+app.get('/',(req,res)=>{
+    const posts = readPost();
+    res.render("index.ejs", {posts});
+});
+
+app.get('/blogs',(req,res)=>{
+    const posts = readPost();
+    setTimeout(()=>{
+        res.render("blogs.ejs", {posts})
+    },1000)
+});
+
+app.get("/create",(req,res)=>{
+    res.render("create.ejs");
+});
+
+app.post('/submit',(req,res)=>{
+    const posts = readPost();
+    const newPosts = {
+        title: req.body.title,
+        image: req.body.image, 
+        description: req.body.description
+    };
+    posts.push(newPosts);
+    writePost(posts);
+    res.redirect('/');
+});
+
+app.post("/delete/:index",(req,res)=>{
+    const posts = readPost();
+    posts.splice(req.params.index, 1);
+    writePost(posts);
+    res.redirect("/")
+})
+
+app.use((req, res) => {
+    setTimeout(()=>{
+        res.status(404).render("404.ejs");
+    },1000)
+});
+
+app.listen(port,()=>{
+    console.log(`This is listening on port ${port}`);
+});
